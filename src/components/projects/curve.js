176fa023d7834aa86652b319be5e4b59f05701b7
Cory Henderson <chenderson19899@gmail.com>
2019-08-14 11:04:27 -0700

    development branch

--- src/components/projects/curve.js
+++ src/components/projects/curve.js
@@ -2,25 +2,15 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 // import Img from "gatsby-image"
 import CurveImage from '../../images/curvelogo.jpeg'
+import Github from '../../images/github.png'
 
-const Curve = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "curve.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => 
+const Curve = () => {
+
+    return( 
       <div className='curve'>                
           <h2>Curve</h2>
           <div id="links">
-            <a href="https://github.com/CodeFellows-Curve/curve-front-end">Github</a>
+            <a href="https://github.com/CodeFellows-Curve/curve-front-end" target="_blank" rel="noopener noreferrer">Github</a>
             <a href="https://codefellows-curve.netlify.com/">Deployed Site</a>
           </div>
           <p className="project-description">An employee development and metrics application.</p>
@@ -37,7 +27,7 @@ const Curve = () => (
           </figure>
           {/* <Img fluid={data.placeholderImage.childImageSharp.fluid} /> */}
       </div>
-    }
-  />
-)
+  )
+}
+
 export default Curve
